64fafaa79134c4f73406f4882a0ef12fc69a8a20
Vishav Kumar <vishav_kumar@Vishavs-MBP-2.ultrahub>
2024-10-10 14:55:13 +0100

    Second commit to lab branch

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     Returns:
         string: greeting response
     """
-    print('Starting functions\n---------------------------------------------'
+    print('Starting functions\n---------------------------------------------')
 
     if event["input"] == "Hello":
 
@@ -16,4 +16,4 @@ def lambda_handler(event, context):
 
     else:
 
-        raise
+        raise Exception("Input was not 'Hello'")  # Raising a generic exception
